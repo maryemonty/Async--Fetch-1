72bcc536c4861be7ace35fed5ca229521deb19c2
maryemonty <113999089+maryemonty@users.noreply.github.com>
2023-03-15 15:31:41 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+fetch('https://striveschool-api.herokuapp.com/books')
+    .then(r => r.json())
+    .then(books => {
+        const grid = document.getElementById('books-container');
+        grid.innerHTML = ''
+
+        books.forEach((books) => {
+            const col = document.createElement('div');
+            col.className = 'col'
+            col.innerHTML = `
+            <div class="card m-3" style="min-height: 600px; max-height: 600px;">
+            <img style="min-height: 300px; max-height: 300px;" src="${books.img}" class="card-img-top">
+                <div class="card-body">
+                    <h5 class="card-title">${books.title}</h5>
+                    <p class="card-text">${books.price} $</p>
+                    <button id="btn" class="btn btn-primary">Scarta</button>
+                </div>
+            </div>`
+            grid.append(col)
+        });
+    })
+    .catch(error => console.log("CATCH", error))
+
